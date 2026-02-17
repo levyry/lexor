c5f899d9773c3bfbae8e3c9cbae59064fe43937f
Orbán Levente <orban.levente.laszlo@gmail.com>
2026-02-17 17:40:13 +0100

    implement base graph reducer
    this graph reducer is still call-by-need, so it doesn't reduce all of
    it's terms. the arena is hard-coded in and the impl is still a bit messy

--- lexor-compiler/src/lib.rs
+++ lexor-compiler/src/lib.rs
@@ -6,6 +6,9 @@ use std::{
     ops::BitAnd,
 };
 
+mod node;
+mod ski_parser;
+
 // data Deb = Zero | Succ Deb | Lam Deb | App Deb Deb deriving Show
 #[derive(Debug, Clone)]
 enum Deb {
---
+++ lexor-compiler/src/node.rs
@@ -0,0 +1,17 @@
+slotmap::new_key_type! {
+    /// A key to access Nodes in the AST Arena
+    pub struct NodeKey;
+}
+
+#[rustfmt::skip]
+/// A node in the AST
+#[derive(Debug, Clone)]
+pub enum Node {
+    /// Regular Combinators
+    S, K, I, B, C,
+    /// Bulk combinators
+    Sn(u8), Bn(u8), Cn(u8),
+    /// Graph structure
+    App(NodeKey, NodeKey),
+    Indirection(NodeKey),
+}
